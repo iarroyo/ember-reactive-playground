da117939591660fb7f2c717a4112b3e709cf3e84
Ivan <iarroyo@copyright.com>
2021-11-29 21:34:05 +0100

    playing with useEffect

--- app/components/toggle/component.js
+++ app/components/toggle/component.js
@@ -1,9 +1,21 @@
-import { useState, useCallback } from 'ember-function-component';
+import { useState, useCallback, useEffect } from 'ember-function-component';
+let effectCounter = 0;
 export default function useToggle() {
   let [value, setValue] = useState(false);
   const toggle = useCallback(() => {
     value = !value;
     setValue(value);
   });
-  return { value, toggle };
+
+  let [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (effectCounter > 0) {
+      effectCounter = 0;
+      return;
+    }
+    setCount(count + 1);
+    effectCounter = effectCounter + 1;
+  });
+  return { value, toggle, count };
 }
